2c5ce1126f823d28a11d7b4213c807fd47fa846e
hezix <hezix@>
2025-04-06 14:52:01 +0300

    first commit

---
+++ src/main/java/com/hezix/shaudifymain/ShaudifyMainApplication.java
@@ -0,0 +1,13 @@
+package com.hezix.shaudifymain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShaudifyMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShaudifyMainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hezix/shaudifymain/ShaudifyMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hezix.shaudifymain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShaudifyMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
