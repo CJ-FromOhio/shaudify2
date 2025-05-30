0b7abfb13f06df3b7389fcb91915b736a78aa94c
hezix <lev281010@protonmail.com>
2025-05-30 19:42:15 +0300

    updated main and login page

--- src/main/java/com/hezix/shaudifymain/controller/web/MainController.java
+++ src/main/java/com/hezix/shaudifymain/controller/web/MainController.java
@@ -1,16 +1,25 @@
 package com.hezix.shaudifymain.controller.web;
 
 import com.hezix.shaudifymain.annotations.CustomControllerAdviceAnnotation;
+import com.hezix.shaudifymain.service.UserService;
+import com.sun.security.auth.UserPrincipal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping()
 @CustomControllerAdviceAnnotation
+@RequiredArgsConstructor
 public class MainController {
+    private final UserService userService;
     @GetMapping()
-    public String main() {
+    public String main(@AuthenticationPrincipal UserDetails userDetails, Model model) {
+        model.addAttribute("user", userService.findUserByUsername(userDetails.getUsername()));
         return "main";
     }
 
